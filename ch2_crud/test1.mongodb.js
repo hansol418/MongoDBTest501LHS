5ef1bee4a6b574a3123d0d831e66b91d95068fe7
LeeHanSol <hslee4954@gmail.com>
2024-07-23 12:49:25 +0900

    몽고DB CRUD

--- ch2_crud/test1.mongodb.js
+++ ch2_crud/test1.mongodb.js
@@ -8,8 +8,8 @@ db.cappedC.find();
 //연습2
 use("testDB");
 // 특정 크기의 컬렉션을 생성, 용량이 초과하면, 오래된 데이터 부터 삭제함. 확인.
-db.createCollection("cappedC2", { capped: true, size: 10000 });
-use("testDB");
+db.createCollection("cappedC2");
+// use("testDB");
 db.cappedC2.insertOne({ x: 1 });
 db.cappedC2.find();
 
@@ -17,28 +17,28 @@ db.cappedC2.find();
 // use("testDB");
 // db.cappedC.stats();
 
-use("testDB");
+// use("testDB");
 for (i = 0; i < 1000; i++) {
   db.cappedC.insertOne({ x: i });
 }
 
 //연습2
-use("testDB");
+// use("testDB");
 for (i = 0; i < 1000; i++) {
   db.cappedC2.insertOne({ x: i });
 }
 
-use("testDB");
+// use("testDB");
 // 앞에, 1 부터 646 까지 다 삭제됨.
 // 오래된 데이텀.
 db.cappedC.find();
 
-use("testDB");
+// use("testDB");
 // 앞에, 1 부터 646 까지 다 삭제됨.
 // 오래된 데이터.
 db.cappedC2.find();
 
 // 서버 상태 조회.
-use("testDB");
+// use("testDB");
 // db.getCollectionInfos();
 db.serverStatus();
